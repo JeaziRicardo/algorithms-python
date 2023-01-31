a4284838580c51f4068116762df71d52d121c34e
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-01-31 16:19:49 -0300

    feat(02): implementa os testes de encrypy_message em caso de sucesso

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -7,3 +7,8 @@ def test_encrypt_message():
         encrypt_message([], 0)
     with pytest.raises(TypeError):
         encrypt_message("message", [])
+
+    assert encrypt_message("message", -1) == "egassem"
+    assert encrypt_message("message", 10) == "egassem"
+    assert encrypt_message("message", 3) == "sem_egas"
+    assert encrypt_message("message", 4) == "ega_ssem"
