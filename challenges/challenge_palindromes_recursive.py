0b60e692151acee9984853a2990e363ad0617531
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-02-01 15:38:58 -0300

    feat(03): implementa a função is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    elif high_index == 0:
+        return True
+    elif word[low_index].lower() != word[high_index].lower():
+        return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
