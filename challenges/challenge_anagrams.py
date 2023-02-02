64bd0115c122158e2834446f3363b1214017002d
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-02-02 11:18:49 -0300

    feat(04): código com comentários

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,53 @@
+# baseado no course
+def merge_sort(left, right, string):
+    char_list = list(string)
+    left_part, right_part = 0, 0
+
+    while left_part < len(left) and right_part < len(right):
+
+        if left[left_part] <= right[right_part]:
+            char_list[left_part + right_part] = left[left_part]
+            left_part += 1
+        else:
+            char_list[left_part + right_part] = right[right_part]
+            right_part += 1
+
+    for left_part in range(left_part, len(left)):
+        char_list[left_part + right_part] = left[left_part]
+
+    for right_part in range(right_part, len(right)):
+        char_list[left_part + right_part] = right[right_part]
+
+    return "".join(char_list)
+
+
+def split_string(string):
+    # dividindo strings
+    if len(string) < 2:
+        return string
+
+    mid = len(string) // 2
+    left = split_string(string[:mid])
+    right = split_string(string[mid:])
+
+    return merge_sort(left, right, string)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # convertendo strings para lowercase para ser case insensitive
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+
+    # atribuindo a uma variável
+    sorted_first_string = split_string(first_string)
+    sorted_second_string = split_string(second_string)
+
+    # verificando se as strings são anagramas
+    if not sorted_first_string or not sorted_second_string:
+        return (sorted_first_string, sorted_second_string, False)
+
+    return (
+        sorted_first_string,
+        sorted_second_string,
+        sorted_first_string == sorted_second_string,
+    )
